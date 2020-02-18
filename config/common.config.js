c0438c9779f5e67f07b87afc0fc11706a9d8c05c
IERomanov <Romanov.i@digdes.com>
2020-02-18 11:36:31 +0400

    feature: use webpack bundle analyzer for build

--- config/common.config.js
+++ config/common.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
 	entry: ['./src/index.js'],
@@ -6,6 +7,9 @@ module.exports = {
 		path: path.resolve(__dirname, '../dist'),
 		filename: 'tds.js',
 	},
+	plugins: [
+		new BundleAnalyzerPlugin()
+	],
 	module: {
 		rules: [
 			{
